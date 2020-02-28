d26622d132d31a912e5441566d44184e57d6c5e3
wluisaraujo <w.luis.araujo@gmail.com>
2020-02-28 17:11:02 -0300

    update

--- script/03-vboxmanage-import-vms.sh
+++ script/03-vboxmanage-import-vms.sh
@@ -12,14 +12,14 @@ if [[ -z ${CURSO} ]]
 	export BASEDIR=/home/repositorio/FREQUENTES/${CURSO} && cd ${BASEDIR}
 	ls -ld $BASEDIR > /dev/null 2>&1
 # 	import appliance
-	if [ $? == 0 ]
+#	if [ $? == 0 ]
+if [ -d /home/repositorio/FREQUENTES/${CURSO} ]
 	  then
 	    for OVA in $(ls -1 ${BASEDIR}/*${CURSO}*.ova)
 	      do
-		echo " Importando appliance ${OVA}"
+		      echo " Importando appliance ${OVA}"
 	        vboxmanage import $OVA
 	      done
-
 #	create vbox host only nic vbox0
 		vboxmanage hostonlyif create && vboxmanage hostonlyif ipconfig vboxnet0 --ip 200.100.50.254 --netmask 255.255.255.0
 		vboxmanage dhcpserver modify --ifname vboxnet0 --enable
@@ -30,25 +30,26 @@ if [[ -z ${CURSO} ]]
 		vboxmanage dhcpserver modify --ifname vboxnet1 --enable
 		vboxmanage dhcpserver modify --ifname vboxnet1 --enable --ip 172.16.100.254 --netmask 255.255.255.0 --lowerip 172.16.100.240 --upperip 172.16.100.253
 
-	      for VM in $(vboxmanage list vms | awk '{ print $1}' | sed s/\"//g )
-		do
-		  vboxmanage modifyvm ${VM} --memory 512
-		  vboxmanage modifyvm ${VM} --pae on
-		  vboxmanage modifyvm ${VM} --cpus 2
-		  vboxmanage modifyvm ${VM} --groups "/${CURSO}"
-	        done
-	        vboxmanage modifyvm Security --nic1 nat
-	        vboxmanage modifyvm Security --nic2 hostonly --hostonlyadapter2 vboxnet1
+      for VM in $(vboxmanage list vms | awk '{ print $1}' | sed s/\"//g )
+		    do
+		      vboxmanage modifyvm ${VM} --memory 512
+		      vboxmanage modifyvm ${VM} --pae on
+		      vboxmanage modifyvm ${VM} --cpus 2
+		      vboxmanage modifyvm ${VM} --groups "/${CURSO}"
+	      done
+
+    vboxmanage modifyvm Security --nic1 nat
+	  vboxmanage modifyvm Security --nic2 hostonly --hostonlyadapter2 vboxnet1
 		vboxmanage modifyvm Security --nic3 hostonly --hostonlyadapter3 vboxnet0
-		for GUEST in Cliente-Externo Lab-Gamification-4752
-		  do
-		    vboxmanage modifyvm ${GUEST} --nic1 nat
-		    vboxmanage modifyvm ${GUEST} --nic2 hostonly --hostonlyadapter2 vboxnet0
-                  done
-	        for GUEST in Storage Web-Server01 Web-Server02 Proxy-Server Mail-Server DevOps
-		  do
-		    vboxmanage modifyvm ${GUEST} --nic1 hostonly --hostonlyadapter1 vboxnet1
-                  done
+		  for GUEST in Cliente-Externo Lab-Gamification-4752
+		    do
+		      vboxmanage modifyvm ${GUEST} --nic1 nat
+		      vboxmanage modifyvm ${GUEST} --nic2 hostonly --hostonlyadapter2 vboxnet0
+        done
+	    for GUEST in Storage Web-Server01 Web-Server02 Proxy-Server Mail-Server DevOps
+		    do
+		      vboxmanage modifyvm ${GUEST} --nic1 hostonly --hostonlyadapter1 vboxnet1
+        done
 ##          for DISK in $(seq 5)
 ##            do
 ##        vboxmanage createmedium disk --filename ~"Virtual Box"/StorageDISK${DISK}.vdi --size 5120
