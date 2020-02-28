d6357bedc4d7e1ec3e5c2d61860f3235b7494306
wluisaraujo <w.luis.araujo@gmail.com>
2020-02-28 19:21:16 -0300

    update

--- script/00-752-FirstDay.sh
+++ script/00-752-FirstDay.sh
@@ -1,7 +1,6 @@
 #!/bin/env bash
-
-#------------------------------------------------------------------------------#
-printf  "remove all vms \n"
+export CURSO="752"
+printf  "Remove all vms \n"
 
 vboxmanage list vms | awk '{ print $1}' | sed s/\"//g > /tmp/vms.old
 
@@ -12,7 +11,7 @@ if [ -f /tmp/vms.old ]
         vboxmanage unregistervm $OLD --delete
       done
 #------------------------------------------------------------------------------#
-    printf  "remove all host only interfaces \n"
+    printf  "Remove all host only interfaces \n"
 
     for vnic in $(vboxmanage list hostonlyifs | grep ^Name | awk '{ print $2}')
       do
@@ -24,46 +23,48 @@ fi
 #------------------------------------------------------------------------------#
 printf "create vbox host only nic vbox0 \n"
 
-vboxmanage hostonlyif create
-vboxmanage hostonlyif ipconfig vboxnet0 \
+vboxmanage hostonlyif create && vboxmanage hostonlyif ipconfig vboxnet0 \
 --ip 200.100.50.254 --netmask 255.255.255.0
 #------------------------------------------------------------------------------#
 printf "configure address vboxnet0 \n"
 
 vboxmanage dhcpserver modify --ifname vboxnet0 --enable \
---ip 200.100.50.254 --netmask 255.255.255.0 --lowerip 200.100.50.240 --upperip 200.100.50.253
+--ip 200.100.50.254 --netmask 255.255.255.0 \
+--lowerip 200.100.50.240 --upperip 200.100.50.253
 #------------------------------------------------------------------------------#
 printf "create vbox host only nic vbox1 \n"
 
-vboxmanage hostonlyif create
-vboxmanage hostonlyif ipconfig vboxnet1 \
+vboxmanage hostonlyif create && vboxmanage hostonlyif ipconfig vboxnet1 \
 --ip 172.16.100.254 --netmask 255.255.255.0
 #------------------------------------------------------------------------------#
 printf "configure address vboxnet1  \n"
 
 vboxmanage dhcpserver modify --ifname vboxnet1 --enable \
---ip 172.16.100.254 --netmask 255.255.255.0 --lowerip 172.16.100.240 --upperip 172.16.100.253
+--ip 172.16.100.254 --netmask 255.255.255.0 \
+--lowerip 172.16.100.240 --upperip 172.16.100.253
 #------------------------------------------------------------------------------#
 
-if [ -d /home/repositorio/FREQUENTES/752 ]
+if [ -d /home/repositorio/FREQUENTES/${CURSO} ]
   then
-    for OVA in $(ls -1 /home/repositorio/FREQUENTES/752/*752*.ova)
+    for OVA in $(ls -1 /home/repositorio/FREQUENTES/${CURSO}/*${CURSO}*.ova)
 	    do
 		    echo " Importing appliance ${OVA}"
 	      vboxmanage import $OVA
 	    done
     printf "create vbox host only nic vbox0 \n"
-		vboxmanage hostonlyif create && vboxmanage hostonlyif ipconfig vboxnet0 --ip 200.100.50.254 --netmask 255.255.255.0
+		vboxmanage hostonlyif create && vboxmanage hostonlyif ipconfig vboxnet0 \
+    --ip 200.100.50.254 --netmask 255.255.255.0
 		vboxmanage dhcpserver modify --ifname vboxnet0 --enable
-		vboxmanage dhcpserver modify --ifname vboxnet0 --enable --ip 200.100.50.254 --netmask 255.255.255.0 --lowerip 200.100.50.240 --upperip 200.100.50.253
+		vboxmanage dhcpserver modify --ifname vboxnet0 --enable --ip 200.100.50.254 \
+    --netmask 255.255.255.0 --lowerip 200.100.50.240 --upperip 200.100.50.253
 #------------------------------------------------------------------------------#
     printf "create vbox host only nic vbox1 \n"
 
     vboxmanage hostonlyif create && vboxmanage hostonlyif ipconfig vboxnet1 \
     --ip 172.16.100.254 --netmask 255.255.255.0
 		vboxmanage dhcpserver modify --ifname vboxnet1 --enable
-		vboxmanage dhcpserver modify --ifname vboxnet1 --enable \
-    --ip 172.16.100.254 --netmask 255.255.255.0 --lowerip 172.16.100.240 --upperip 172.16.100.253
+		vboxmanage dhcpserver modify --ifname vboxnet1 --enable --ip 172.16.100.254 \
+    --netmask 255.255.255.0 --lowerip 172.16.100.240 --upperip 172.16.100.253
 #------------------------------------------------------------------------------#
     printf "Adjusting CPU/Memory \n"
 
@@ -72,7 +73,7 @@ if [ -d /home/repositorio/FREQUENTES/752 ]
         vboxmanage modifyvm ${VM} --memory 512
         vboxmanage modifyvm ${VM} --pae on
         vboxmanage modifyvm ${VM} --cpus 2
-        vboxmanage modifyvm ${VM} --groups "/752"
+        vboxmanage modifyvm ${VM} --groups "/${CURSO}"
 	    done
 #------------------------------------------------------------------------------#
     printf "Adjusting NIC to host Security \n"
@@ -95,20 +96,20 @@ if [ -d /home/repositorio/FREQUENTES/752 ]
         done
 fi
 #------------------------------------------------------------------------------#
-cd $HOME && mkdir -pv "$HOME/VirtualBox VMs/752/Storage"
+cd $HOME && mkdir -pv "$HOME/VirtualBox VMs/${CURSO}/Storage"
 
-if [ -d "$HOME/VirtualBox VMs/752/Storage" ]
+if [ -d "$HOME/VirtualBox VMs/${CURSO}/Storage" ]
   then
     for D in $(seq 10 15)
       do
-        cd "$HOME/VirtualBox VMs/752/Storage"
+        cd "$HOME/VirtualBox VMs/${CURSO}/Storage"
         vboxmanage createmedium disk --filename \
-        "$HOME/VirtualBox VMs/752/Storage/Disk${D}.vdi" \
-        --size 5120 --format VDI --variant Standard
-        vboxmanage storageattach "Storage" --storagectl "SATA Controller" \
+        "$HOME/VirtualBox VMs/${CURSO}/Storage/Disk${D}.vdi" \
+        --size 10240 --format VDI --variant Standard \
+        && vboxmanage storageattach "Storage" --storagectl "SATA Controller" \
         --port ${D} --type hdd \
-        --medium "$HOME/VirtualBox VMs/752/Storage/Disk${D}.vdi"
+        --medium "$HOME/VirtualBox VMs/${CURSO}/Storage/Disk${D}.vdi"
     done
   else
-    printf "VM not found \n" && exit 1
+    printf "404 VM not found \n" && exit 1
 fi
